79070bffd15f3653e5f9bd592aafddc63aeb5517
LukeChuang <zhuang890212@gmail.com>
2021-01-10 12:34:22 +0800

    first commit

---
+++ Cognitive-Model/layers.py
@@ -0,0 +1,101 @@
+#%%
+import tensorflow as tf 
+import connections as conn 
+
+#%%
+class input_layer(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(input_layer, self).__init__()
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    self.kernel = self.add_weight("kernel",
+                                  shape=[int(input_shape[-1]),
+                                         self.num_outputs])
+
+  def call(self, input):
+    return tf.matmul(input, self.kernel)
+
+#%%
+class v1(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(v1, self).__init__()
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    self.kernel = self.add_weight("kernel",
+                                  shape=[int(input_shape[-1]),
+                                         self.num_outputs])
+
+  def call(self, input):
+    return tf.matmul(input, self.kernel)
+
+#%%
+class spat1(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(spat1, self).__init__()
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    self.kernel = self.add_weight("kernel",
+                                  shape=[int(input_shape[-1]),
+                                         self.num_outputs])
+
+  def call(self, input):
+    return tf.matmul(input, self.kernel)
+
+#%%
+class spat2(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(spat2, self).__init__()
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    self.kernel = self.add_weight("kernel",
+                                  shape=[int(input_shape[-1]),
+                                         self.num_outputs])
+
+  def call(self, input):
+    return tf.matmul(input, self.kernel)
+
+#%%
+class obj1(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(obj1, self).__init__()
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    self.kernel = self.add_weight("kernel",
+                                  shape=[int(input_shape[-1]),
+                                         self.num_outputs])
+
+  def call(self, input):
+    return tf.matmul(input, self.kernel)
+
+#%%
+class obj2(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(obj2, self).__init__()
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    self.kernel = self.add_weight("kernel",
+                                  shape=[int(input_shape[-1]),
+                                         self.num_outputs])
+
+  def call(self, input):
+    return tf.matmul(input, self.kernel)
+
+#%%
+class output_layer(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(output_layer, self).__init__()
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    self.kernel = self.add_weight("kernel",
+                                  shape=[int(input_shape[-1]),
+                                         self.num_outputs])
+
+  def call(self, input):
+    return tf.matmul(input, self.kernel)
---
+++ Cognitive-Model/test.py
@@ -0,0 +1,165 @@
+#%%
+import tensorflow as tf 
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+import numpy as np
+
+#%%
+class MyModel(Model):
+  def __init__(self):
+    super(MyModel, self).__init__()
+    self.conv1 = Conv2D(32, 3, activation='relu')
+    self.flatten = Flatten()
+    self.d1 = Dense(128, activation='relu')
+    self.d2 = Dense(10)
+
+    print(self.d1)
+  def call(self, x):
+    x = self.conv1(x)
+    x = self.flatten(x)
+    x = self.d1(x)
+    return self.d2(x)
+
+# Create an instance of the model
+model = MyModel()
+
+#%%
+x = tf.random.uniform([3,3])
+
+print("Is there a GPU available: "),
+print(tf.config.experimental.list_physical_devices("GPU"))
+
+print("Is the Tensor on GPU #0:  "),
+print(x.device.endswith('GPU:0'))
+
+#%%
+import time
+
+def time_matmul(x):
+  start = time.time()
+  for loop in range(10):
+    tf.matmul(x, x)
+
+  result = time.time()-start
+
+  print("10 loops: {:0.2f}ms".format(1000*result))
+
+# Force execution on CPU
+print("On CPU:")
+with tf.device("CPU:0"):
+  x = tf.random.uniform([1000, 1000])
+  assert x.device.endswith("CPU:0")
+  time_matmul(x)
+
+# Force execution on GPU #0 if available
+if tf.config.experimental.list_physical_devices("GPU"):
+  print("On GPU:")
+  with tf.device("GPU:0"): # Or GPU:1 for the 2nd GPU, GPU:2 for the 3rd etc.
+    x = tf.random.uniform([1000, 1000])
+    assert x.device.endswith("GPU:0")
+    time_matmul(x)
+
+#%%
+ds_tensors = tf.data.Dataset.from_tensor_slices([1, 2, 3, 4, 5, 6])
+
+# Create a CSV file
+import tempfile
+_, filename = tempfile.mkstemp()
+
+with open(filename, 'w') as f:
+  f.write("""Line 1
+Line 2
+Line 3
+  """)
+
+ds_file = tf.data.TextLineDataset(filename)
+
+#%%
+ds_tensors = ds_tensors.map(tf.square).shuffle(2).batch(2)
+
+ds_file = ds_file.batch(2)
+
+#%%
+print('Elements of ds_tensors:')
+for x in ds_tensors:
+  print(x)
+
+print('\nElements in ds_file:')
+for x in ds_file:
+  print(x)
+
+#%%
+print(tf.test.is_gpu_available())
+
+#%%
+# In the tf.keras.layers package, layers are objects. To construct a layer,
+# simply construct the object. Most layers take as a first argument the number
+# of output dimensions / channels.
+layer = tf.keras.layers.Dense(100)
+# The number of input dimensions is often unnecessary, as it can be inferred
+# the first time the layer is used, but it can be provided if you want to
+# specify it manually, which is useful in some complex models.
+layer = tf.keras.layers.Dense(10, input_shape=(None, 5))
+
+layer(tf.zeros([10, 5]))
+
+# print(layer.variables)
+# print(layer.kernel)
+# print(layer.bias)
+
+#%%
+class MyDenseLayer(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(MyDenseLayer, self).__init__()
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    self.kernel = self.add_weight("kernel",
+                                  shape=[int(input_shape[-1]),
+                                         self.num_outputs])
+
+  def call(self, input):
+    return tf.matmul(input, self.kernel)
+
+layer = MyDenseLayer(10)
+layer(tf.zeros([10, 5]))
+
+# print(layer.variables)
+# print(layer.trainable_variables)
+print([var.name for var in layer.trainable_variables])
+
+#%%
+class ResnetIdentityBlock(tf.keras.Model):
+  def __init__(self, kernel_size, filters):
+    super(ResnetIdentityBlock, self).__init__(name='')
+    filters1, filters2, filters3 = filters
+
+    self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1))
+    self.bn2a = tf.keras.layers.BatchNormalization()
+
+    self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same')
+    self.bn2b = tf.keras.layers.BatchNormalization()
+
+    self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1))
+    self.bn2c = tf.keras.layers.BatchNormalization()
+
+  def call(self, input_tensor, training=False):
+    x = self.conv2a(input_tensor)
+    x = self.bn2a(x, training=training)
+    x = tf.nn.relu(x)
+
+    x = self.conv2b(x)
+    x = self.bn2b(x, training=training)
+    x = tf.nn.relu(x)
+
+    x = self.conv2c(x)
+    x = self.bn2c(x, training=training)
+
+    x += input_tensor
+    return tf.nn.relu(x)
+
+
+block = ResnetIdentityBlock(1, [1, 2, 3])
+block(tf.zeros([1, 2, 3, 3]))
+block.variables
