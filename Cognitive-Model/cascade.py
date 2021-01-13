1f8fe6cd0c884b83956a935ca4cae90e6b8bf90f
LukeChuang <zhuang890212@gmail.com>
2021-01-13 21:44:39 +0800

    add cascade.py

---
+++ Cognitive-Model/cascade.py
@@ -0,0 +1,86 @@
+#%%
+from connections import tf, np
+
+class Cascade():
+#%%
+    def __init__(self, bias):
+        # 調整bias的地方
+        Cascade.bias = bias # currently, used only in obj2_to_output
+
+# 調整每一種層和層之間訊息傳遞的運算方式
+#%%
+    def two_d_softmax(self,tensor):
+        two_d_arr = tensor.numpy() 
+        shape = two_d_arr.shape
+        one_d_arr = two_d_arr.flatten()
+        arr = tf.nn.softmax(one_d_arr.astype(float)).numpy().reshape(list(shape))
+        return tf.convert_to_tensor(arr,dtype=tf.dtypes.float32)
+
+    def two_d_sigmoid(self,tensor):
+        two_d_arr = tensor.numpy() 
+        shape = two_d_arr.shape
+        one_d_arr = two_d_arr.flatten()
+        arr = tf.nn.sigmoid(one_d_arr.astype(float)).numpy().reshape(list(shape))
+        return tf.convert_to_tensor(arr,dtype=tf.dtypes.float32)
+
+# 調整每一種層和層之間訊息傳遞的運算方式
+#%%
+    def input_to_v1(self,classname):
+        classname.v1_arr = ((self.two_d_softmax(classname.input_layer(classname.input_tensor))+classname.v1_arr)*(1-classname.cascade_rate)+classname.v1_arr*classname.cascade_rate)
+        # classname.v1_arr = (classname.input_layer(classname.input_tensor)+classname.v1_arr)*(1-classname.cascade_rate)+classname.v1_arr*classname.cascade_rate
+
+    def v1_to_spat1(self,classname):
+        # classname.spat1_arr = ((self.two_d_softmax(classname.v1(classname.v1_arr, "Spat1"))+classname.spat1_arr)*(1-classname.cascade_rate)+classname.spat1_arr*classname.cascade_rate)
+        classname.spat1_arr = (classname.v1(classname.v1_arr, "Spat1")+classname.spat1_arr)*(1-classname.cascade_rate)+classname.spat1_arr*classname.cascade_rate
+    def spat1_to_v1(self,classname):
+        classname.v1_arr = (self.two_d_softmax(classname.spat1(classname.spat1_arr, "V1"))+classname.v1_arr)*(1-classname.cascade_rate)+classname.v1_arr*classname.cascade_rate
+        # classname.v1_arr = (classname.spat1(classname.spat1_arr, "V1")+classname.v1_arr)*(1-classname.cascade_rate)+classname.v1_arr*classname.cascade_rate
+
+    def spat1_to_spat2(self,classname):  
+        # classname.spat2_arr = ((self.two_d_softmax(classname.spat1(classname.spat1_arr, "Spat2"))+classname.spat2_arr)*(1-classname.cascade_rate)+classname.spat2_arr*classname.cascade_rate)
+        classname.spat2_arr = (classname.spat1(classname.spat1_arr, "Spat2")+classname.spat2_arr)*(1-classname.cascade_rate)+classname.spat2_arr*classname.cascade_rate
+    def spat2_to_spat1(self,classname):
+        classname.spat1_arr = (self.two_d_softmax(classname.spat2(classname.spat2_arr, "Spat1"))+classname.spat1_arr)*(1-classname.cascade_rate)+classname.spat1_arr*classname.cascade_rate
+        # classname.spat1_arr = (classname.spat2(classname.spat2_arr, "Spat1")+classname.spat1_arr)*(1-classname.cascade_rate)+classname.spat1_arr*classname.cascade_rate
+
+    def v1_to_obj1(self,classname):
+        classname.obj1_arr = ((self.two_d_softmax(classname.v1(classname.v1_arr, "Obj1"))+classname.obj1_arr)*(1-classname.cascade_rate)+classname.obj1_arr*classname.cascade_rate)
+        # classname.obj1_arr = (classname.v1(classname.v1_arr, "Obj1")+classname.obj1_arr)*(1-classname.cascade_rate)+classname.obj1_arr*classname.cascade_rate
+    def obj1_to_v1(self,classname):
+        # classname.v1_arr = (self.two_d_softmax(classname.obj1(classname.obj1_arr, "V1"))+classname.v1_arr)*(1-classname.cascade_rate)+classname.v1_arr*classname.cascade_rate
+        classname.v1_arr = (classname.obj1(classname.obj1_arr, "V1")+classname.v1_arr)*(1-classname.cascade_rate)+classname.v1_arr*classname.cascade_rate
+
+    def obj1_to_obj2(self,classname):   
+        classname.obj2_arr = ((self.two_d_softmax(classname.obj1(classname.obj1_arr, "Obj2"))+classname.obj2_arr)*(1-classname.cascade_rate)+classname.obj2_arr*classname.cascade_rate)
+        # classname.obj2_arr = (classname.obj1(classname.obj1_arr, "Obj2")+classname.obj2_arr)*(1-classname.cascade_rate)+classname.obj2_arr*classname.cascade_rate
+    def obj2_to_obj1(self,classname):
+        # classname.obj1_arr = (self.two_d_softmax(classname.obj2(classname.obj2_arr, "Obj1"))+classname.obj1_arr)*(1-classname.cascade_rate)+classname.obj1_arr*classname.cascade_rate
+        classname.obj1_arr = (classname.obj2(classname.obj2_arr, "Obj1")+classname.obj1_arr)*(1-classname.cascade_rate)+classname.obj1_arr*classname.cascade_rate
+
+    def spat1_to_obj1(self,classname):
+        classname.obj1_arr = ((self.two_d_softmax(classname.spat1(classname.spat1_arr, "Obj1"))+classname.obj1_arr)*(1-classname.cascade_rate)+classname.obj1_arr*classname.cascade_rate)
+        # classname.obj1_arr = (classname.spat1(classname.spat1_arr, "Obj1")+classname.obj1_arr)*(1-classname.cascade_rate)+classname.obj1_arr*classname.cascade_rate
+    def obj1_to_spat1(self,classname):
+        # classname.spat1_arr = (self.two_d_softmax(classname.obj1(classname.obj1_arr, "Spat1"))+classname.spat1_arr)*(1-classname.cascade_rate)+classname.spat1_arr*classname.cascade_rate
+        classname.spat1_arr = (classname.obj1(classname.obj1_arr, "Spat1")+classname.spat1_arr)*(1-classname.cascade_rate)+classname.spat1_arr*classname.cascade_rate
+
+    def spat2_to_obj2(self,classname):
+        classname.obj2_arr = ((self.two_d_softmax(classname.spat2(classname.spat2_arr, "Obj2"))+classname.obj2_arr)*(1-classname.cascade_rate)+classname.obj2_arr*classname.cascade_rate)
+        # classname.obj2_arr = (classname.spat2(classname.spat2_arr, "Obj2")+classname.obj2_arr)*(1-classname.cascade_rate)+classname.obj2_arr*classname.cascade_rate
+    def obj2_to_spat2(self,classname):
+        # classname.spat2_arr = (self.two_d_softmax(classname.obj2(classname.obj2_arr, "Spat2"))+classname.spat2_arr)*(1-classname.cascade_rate)+classname.spat2_arr*classname.cascade_rate
+        classname.spat2_arr = (classname.obj2(classname.obj2_arr, "Spat2")+classname.spat2_arr)*(1-classname.cascade_rate)+classname.spat2_arr*classname.cascade_rate
+
+    def obj2_to_output(self,classname):   
+        # classname.output_arr = ((self.two_d_softmax(classname.obj2(classname.obj2_arr, "Output"))+classname.output_arr)*(1-classname.cascade_rate)+classname.output_arr*classname.cascade_rate)-0.05
+        classname.output_arr = ((classname.obj2(classname.obj2_arr, "Output")+classname.output_arr)*(1-classname.cascade_rate)+classname.output_arr*classname.cascade_rate)+Cascade.bias
+    def output_to_obj2(self,classname):      
+        # classname.obj2_arr = (self.two_d_softmax(classname.output_layer(classname.output_arr))+classname.obj2_arr)*(1-classname.cascade_rate)+classname.obj2_arr*classname.cascade_rate
+        classname.obj2_arr = (classname.output_layer(classname.output_arr)+classname.obj2_arr)*(1-classname.cascade_rate)+classname.obj2_arr*classname.cascade_rate 
+
+    def spat1_lateral_inhibit(self,classname):
+        # classname.spat1_arr = (self.two_d_softmax(classname.spat1(classname.spat1_arr, "classname"))+classname.spat1_arr)*(1-classname.cascade_rate)+classname.spat1_arr*classname.cascade_rate
+        classname.spat1_arr = ((classname.spat1(classname.spat1_arr, "self")+classname.spat1_arr)*(1-classname.cascade_rate)+classname.spat1_arr*classname.cascade_rate)
+    def spat2_lateral_inhibit(self,classname):
+        # classname.spat2_arr = (self.two_d_softmax(classname.spat2(classname.spat2_arr, "classname"))+classname.spat2_arr)*(1-classname.cascade_rate)+classname.spat2_arr*classname.cascade_rate
+        classname.spat2_arr = ((classname.spat2(classname.spat2_arr, "self")+classname.spat2_arr)*(1-classname.cascade_rate)+classname.spat2_arr*classname.cascade_rate)
--- Cognitive-Model/network.py
+++ Cognitive-Model/network.py
@@ -3,96 +3,99 @@ from connections import tf, np
 from random import shuffle
 
 import layers
+from cascade import Cascade
 
-# 調整每一種層和層之間訊息傳遞的運算方式
+# # 調整每一種層和層之間訊息傳遞的運算方式
+# #%%
+# def two_d_softmax(tensor):
+#   two_d_arr = tensor.numpy() 
+#   shape = two_d_arr.shape
+#   one_d_arr = two_d_arr.flatten()
+#   arr = tf.nn.softmax(one_d_arr.astype(float)).numpy().reshape(list(shape))
+#   return tf.convert_to_tensor(arr,dtype=tf.dtypes.float32)
+
+# def two_d_sigmoid(tensor):
+#   two_d_arr = tensor.numpy() 
+#   shape = two_d_arr.shape
+#   one_d_arr = two_d_arr.flatten()
+#   arr = tf.nn.sigmoid(one_d_arr.astype(float)).numpy().reshape(list(shape))
+#   return tf.convert_to_tensor(arr,dtype=tf.dtypes.float32)
+
+# # 調整每一種層和層之間訊息傳遞的運算方式
+# #%%
+# # 調整bias的地方
+# bias = -0.5 # currently, used only in obj2_to_output
+
+# def input_to_v1(self):
+#   self.v1_arr = ((two_d_softmax(self.input_layer(self.input_tensor))+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate)
+#   # self.v1_arr = (self.input_layer(self.input_tensor)+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
+
+# def v1_to_spat1(self):
+#   # self.spat1_arr = ((two_d_softmax(self.v1(self.v1_arr, "Spat1"))+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate)
+#   self.spat1_arr = (self.v1(self.v1_arr, "Spat1")+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
+# def spat1_to_v1(self):
+#   self.v1_arr = (two_d_softmax(self.spat1(self.spat1_arr, "V1"))+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
+#   # self.v1_arr = (self.spat1(self.spat1_arr, "V1")+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
+
+# def spat1_to_spat2(self):  
+#   # self.spat2_arr = ((two_d_softmax(self.spat1(self.spat1_arr, "Spat2"))+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate)
+#   self.spat2_arr = (self.spat1(self.spat1_arr, "Spat2")+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate
+# def spat2_to_spat1(self):
+#   self.spat1_arr = (two_d_softmax(self.spat2(self.spat2_arr, "Spat1"))+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
+#   # self.spat1_arr = (self.spat2(self.spat2_arr, "Spat1")+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
+
+# def v1_to_obj1(self):
+#   self.obj1_arr = ((two_d_softmax(self.v1(self.v1_arr, "Obj1"))+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate)
+#   # self.obj1_arr = (self.v1(self.v1_arr, "Obj1")+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate
+# def obj1_to_v1(self):
+#   # self.v1_arr = (two_d_softmax(self.obj1(self.obj1_arr, "V1"))+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
+#   self.v1_arr = (self.obj1(self.obj1_arr, "V1")+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
+
+# def obj1_to_obj2(self):   
+#   self.obj2_arr = ((two_d_softmax(self.obj1(self.obj1_arr, "Obj2"))+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate)
+#   # self.obj2_arr = (self.obj1(self.obj1_arr, "Obj2")+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate
+# def obj2_to_obj1(self):
+#   # self.obj1_arr = (two_d_softmax(self.obj2(self.obj2_arr, "Obj1"))+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate
+#   self.obj1_arr = (self.obj2(self.obj2_arr, "Obj1")+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate
+
+# def spat1_to_obj1(self):
+#   self.obj1_arr = ((two_d_softmax(self.spat1(self.spat1_arr, "Obj1"))+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate)
+#   # self.obj1_arr = (self.spat1(self.spat1_arr, "Obj1")+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate
+# def obj1_to_spat1(self):
+#   # self.spat1_arr = (two_d_softmax(self.obj1(self.obj1_arr, "Spat1"))+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
+#   self.spat1_arr = (self.obj1(self.obj1_arr, "Spat1")+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
+
+# def spat2_to_obj2(self):
+#   self.obj2_arr = ((two_d_softmax(self.spat2(self.spat2_arr, "Obj2"))+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate)
+#   # self.obj2_arr = (self.spat2(self.spat2_arr, "Obj2")+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate
+# def obj2_to_spat2(self):
+#   # self.spat2_arr = (two_d_softmax(self.obj2(self.obj2_arr, "Spat2"))+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate
+#   self.spat2_arr = (self.obj2(self.obj2_arr, "Spat2")+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate
+
+# def obj2_to_output(self):   
+#   # self.output_arr = ((two_d_softmax(self.obj2(self.obj2_arr, "Output"))+self.output_arr)*(1-self.cascade_rate)+self.output_arr*self.cascade_rate)-0.05
+#   self.output_arr = ((self.obj2(self.obj2_arr, "Output")+self.output_arr)*(1-self.cascade_rate)+self.output_arr*self.cascade_rate)+bias
+# def output_to_obj2(self):      
+#   # self.obj2_arr = (two_d_softmax(self.output_layer(self.output_arr))+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate
+#   self.obj2_arr = (self.output_layer(self.output_arr)+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate 
+
+# def spat1_lateral_inhibit(self):
+#   # self.spat1_arr = (two_d_softmax(self.spat1(self.spat1_arr, "self"))+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
+#   self.spat1_arr = ((self.spat1(self.spat1_arr, "self")+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate)
+# def spat2_lateral_inhibit(self):
+#   # self.spat2_arr = (two_d_softmax(self.spat2(self.spat2_arr, "self"))+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate
+#   self.spat2_arr = ((self.spat2(self.spat2_arr, "self")+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate)
+
 #%%
-def two_d_softmax(tensor):
-  two_d_arr = tensor.numpy() 
-  shape = two_d_arr.shape
-  one_d_arr = two_d_arr.flatten()
-  arr = tf.nn.softmax(one_d_arr.astype(float)).numpy().reshape(list(shape))
-  return tf.convert_to_tensor(arr,dtype=tf.dtypes.float32)
-
-def two_d_sigmoid(tensor):
-  two_d_arr = tensor.numpy() 
-  shape = two_d_arr.shape
-  one_d_arr = two_d_arr.flatten()
-  arr = tf.nn.sigmoid(one_d_arr.astype(float)).numpy().reshape(list(shape))
-  return tf.convert_to_tensor(arr,dtype=tf.dtypes.float32)
-
-# 調整每一種層和層之間訊息傳遞的運算方式
-#%%
-# 調整bias的地方
-bias = -0.5 # currently, used only in obj2_to_output
-
-def input_to_v1(self):
-  self.v1_arr = ((two_d_softmax(self.input_layer(self.input_tensor))+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate)
-  # self.v1_arr = (self.input_layer(self.input_tensor)+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
-
-def v1_to_spat1(self):
-  # self.spat1_arr = ((two_d_softmax(self.v1(self.v1_arr, "Spat1"))+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate)
-  self.spat1_arr = (self.v1(self.v1_arr, "Spat1")+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
-def spat1_to_v1(self):
-  self.v1_arr = (two_d_softmax(self.spat1(self.spat1_arr, "V1"))+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
-  # self.v1_arr = (self.spat1(self.spat1_arr, "V1")+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
-
-def spat1_to_spat2(self):  
-  # self.spat2_arr = ((two_d_softmax(self.spat1(self.spat1_arr, "Spat2"))+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate)
-  self.spat2_arr = (self.spat1(self.spat1_arr, "Spat2")+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate
-def spat2_to_spat1(self):
-  self.spat1_arr = (two_d_softmax(self.spat2(self.spat2_arr, "Spat1"))+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
-  # self.spat1_arr = (self.spat2(self.spat2_arr, "Spat1")+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
-
-def v1_to_obj1(self):
-  self.obj1_arr = ((two_d_softmax(self.v1(self.v1_arr, "Obj1"))+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate)
-  # self.obj1_arr = (self.v1(self.v1_arr, "Obj1")+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate
-def obj1_to_v1(self):
-  # self.v1_arr = (two_d_softmax(self.obj1(self.obj1_arr, "V1"))+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
-  self.v1_arr = (self.obj1(self.obj1_arr, "V1")+self.v1_arr)*(1-self.cascade_rate)+self.v1_arr*self.cascade_rate
-
-def obj1_to_obj2(self):   
-  self.obj2_arr = ((two_d_softmax(self.obj1(self.obj1_arr, "Obj2"))+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate)
-  # self.obj2_arr = (self.obj1(self.obj1_arr, "Obj2")+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate
-def obj2_to_obj1(self):
-  # self.obj1_arr = (two_d_softmax(self.obj2(self.obj2_arr, "Obj1"))+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate
-  self.obj1_arr = (self.obj2(self.obj2_arr, "Obj1")+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate
-
-def spat1_to_obj1(self):
-  self.obj1_arr = ((two_d_softmax(self.spat1(self.spat1_arr, "Obj1"))+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate)
-  # self.obj1_arr = (self.spat1(self.spat1_arr, "Obj1")+self.obj1_arr)*(1-self.cascade_rate)+self.obj1_arr*self.cascade_rate
-def obj1_to_spat1(self):
-  # self.spat1_arr = (two_d_softmax(self.obj1(self.obj1_arr, "Spat1"))+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
-  self.spat1_arr = (self.obj1(self.obj1_arr, "Spat1")+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
-
-def spat2_to_obj2(self):
-  self.obj2_arr = ((two_d_softmax(self.spat2(self.spat2_arr, "Obj2"))+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate)
-  # self.obj2_arr = (self.spat2(self.spat2_arr, "Obj2")+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate
-def obj2_to_spat2(self):
-  # self.spat2_arr = (two_d_softmax(self.obj2(self.obj2_arr, "Spat2"))+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate
-  self.spat2_arr = (self.obj2(self.obj2_arr, "Spat2")+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate
-
-def obj2_to_output(self):   
-  # self.output_arr = ((two_d_softmax(self.obj2(self.obj2_arr, "Output"))+self.output_arr)*(1-self.cascade_rate)+self.output_arr*self.cascade_rate)-0.05
-  self.output_arr = ((self.obj2(self.obj2_arr, "Output")+self.output_arr)*(1-self.cascade_rate)+self.output_arr*self.cascade_rate)+bias
-def output_to_obj2(self):      
-  # self.obj2_arr = (two_d_softmax(self.output_layer(self.output_arr))+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate
-  self.obj2_arr = (self.output_layer(self.output_arr)+self.obj2_arr)*(1-self.cascade_rate)+self.obj2_arr*self.cascade_rate 
-
-def spat1_lateral_inhibit(self):
-  # self.spat1_arr = (two_d_softmax(self.spat1(self.spat1_arr, "self"))+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate
-  self.spat1_arr = ((self.spat1(self.spat1_arr, "self")+self.spat1_arr)*(1-self.cascade_rate)+self.spat1_arr*self.cascade_rate)
-def spat2_lateral_inhibit(self):
-  # self.spat2_arr = (two_d_softmax(self.spat2(self.spat2_arr, "self"))+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate
-  self.spat2_arr = ((self.spat2(self.spat2_arr, "self")+self.spat2_arr)*(1-self.cascade_rate)+self.spat2_arr*self.cascade_rate)
-
-#%%
-class VisualNetWork(tf.keras.Model):
+class VisualNetWork(tf.keras.Model, Cascade):
   def __init__(self, wt, cascade_rate): 
     super(VisualNetWork, self).__init__(name='')
     
     layers.Layers(wt)
 
     self.cascade_rate = cascade_rate
+
+    self.cascade = Cascade(-0.5)
 
     self.input_layer = layers.InputLayer()
     self.v1 = layers.V1()
@@ -114,23 +117,23 @@ class VisualNetWork(tf.keras.Model):
     self.input_tensor = input_tensor
 
     # 調整哪些layer之間有連結，可以直接刪掉以切斷連結
-    path_functions = [input_to_v1, #0
-                          v1_to_spat1, #1
-                          spat1_to_v1, #2
-                          spat1_to_spat2, #3
-                          spat2_to_spat1, #4
-                          v1_to_obj1, #5
-                          obj1_to_v1, #6
-                          obj1_to_obj2, #7
-                          obj2_to_obj1, #8
-                          spat1_to_obj1, #9
-                          obj1_to_spat1, #10
-                          spat2_to_obj2, #11
-                          obj2_to_spat2, #12
-                          obj2_to_output, #13
-                          output_to_obj2, #14
-                          spat1_lateral_inhibit, #15
-                          spat2_lateral_inhibit #16
+    path_functions = [self.cascade.input_to_v1, #0
+                          self.cascade.v1_to_spat1, #1
+                          self.cascade.spat1_to_v1, #2
+                          self.cascade.spat1_to_spat2, #3
+                          self.cascade.spat2_to_spat1, #4
+                          self.cascade.v1_to_obj1, #5
+                          self.cascade.obj1_to_v1, #6
+                          self.cascade.obj1_to_obj2, #7
+                          self.cascade.obj2_to_obj1, #8
+                          self.cascade.spat1_to_obj1, #9
+                          self.cascade.obj1_to_spat1, #10
+                          self.cascade.spat2_to_obj2, #11
+                          self.cascade.obj2_to_spat2, #12
+                          self.cascade.obj2_to_output, #13
+                          self.cascade.output_to_obj2, #14
+                          self.cascade.spat1_lateral_inhibit, #15
+                          self.cascade.spat2_lateral_inhibit #16
                           ] 
 
     if iscue:
@@ -146,7 +149,7 @@ class VisualNetWork(tf.keras.Model):
         shuffle(path_functions)
 
         for path_f in path_functions:
-          path_f(self)
+          path_f(self, )
           # Ignore below (for testing) 
           # print(path_f,'\n')
           # print("obj2_arr")
